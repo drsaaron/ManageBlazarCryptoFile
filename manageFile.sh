efaee5cef63a62f665e778e282f2eb24168d8665
AARON SCOTT <scottaaron@northwesternmutual.com>
2017-11-30 14:27:19 -0600

    remove .DS_Store; add a script to run the thing

---
+++ manageFile.sh
@@ -0,0 +1,31 @@
+#! /bin/ksh
+
+while getopts :u:r:p: option
+do
+    case $option in
+	u)
+	    user=$OPTARG
+	    ;;
+	r)
+	    resource=$OPTARG
+	    ;;
+	p)
+	    password=$OPTARG
+	    ;;
+	*)
+	    echo "invalid option $OPTION"
+	    echo "usage: $(basename $0) [-u user -r resource -p password]" 1>&2
+	    exit 1
+    esac
+done
+
+# build command line
+if [[ $# > 0 ]]
+then
+    commandLine="$user $resource $password"
+else
+    commandLine=
+fi
+
+# run
+exec mvn exec:java -Dexec.args="$commandLine"
